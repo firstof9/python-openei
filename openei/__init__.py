e3fee54a05f79b0db1475593b4223ee1ef778c9e
firstof9 <firstof9@gmail.com>
2021-08-13 11:41:27 -0700

    linting

--- openei/__init__.py
+++ openei/__init__.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 
 import datetime
 import logging
-from typing import Optional
-
+from typing import Any, Dict
 import requests  # type: ignore
 from .const import BASE_URL
 
@@ -30,7 +29,13 @@ class APIError(Exception):
 class Rates:
     """Represent OpenEI Rates."""
 
-    def __init__(self, api: str, lat: float, lon: float, plan: str = None) -> None:
+    def __init__(
+        self,
+        api: str,
+        lat: float,
+        lon: float,
+        plan: str = None,
+    ) -> None:
         """Initialize."""
         self._api = api
         self._lat = lat
@@ -38,12 +43,12 @@ class Rates:
         self._plan = plan
         self._data = None
 
-    def lookup_plans(self) -> dict:
+    def lookup_plans(self) -> Dict[str, Any]:
         """Return the rate plan names per utility in the area."""
         url = f"{BASE_URL}version=latest&format=json"
         url = f"{url}&api_key={self._api}&lat={self._lat}&lon={self._lon}"
         url = f"{url}&sector=Residential"
-        rate_names = {}
+        rate_names: Dict[str, Any] = {}
 
         result = requests.get(url)
         if result.status_code == 404:
@@ -58,10 +63,11 @@ class Rates:
 
         if "items" in result.json().keys():
             for item in result.json()["items"]:
-                if item["utility"] not in rate_names.keys():
-                    rate_names[item["utility"]] = []
+                utility: str = item["utility"]
+                if utility not in rate_names.keys():
+                    rate_names[utility] = []
                 info = {"name": item["name"], "label": item["label"]}
-                rate_names[item["utility"]].append(info)
+                rate_names[utility].append(info)
 
         return rate_names
 
@@ -92,11 +98,12 @@ class Rates:
     @property
     def current_rate(self) -> float:
         """Return the current rate."""
+        assert self._data is not None
         weekend = False
-        dt = datetime.datetime.today()
-        month = dt.month - 1
-        hour = dt.hour
-        if dt.weekday() > 4:
+        now = datetime.datetime.today()
+        month = now.month - 1
+        hour = now.hour
+        if now.weekday() > 4:
             weekend = True
         table = "energyweekdayschedule"
         if weekend:
@@ -112,14 +119,17 @@ class Rates:
     @property
     def rate_name(self) -> str:
         """Return the rate name."""
+        assert self._data is not None
         return self._data["name"]
 
     @property
     def approval(self) -> bool:
         """Return the rate name."""
+        assert self._data is not None
         return self._data["approved"]
 
     @property
     def distributed_generation(self) -> str:
         """Return the distributed generation name."""
+        assert self._data is not None
         return self._data["dgrules"]
